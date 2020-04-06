de45f1ecf3882c6b3ef8f55bdb2b960306bc781b
t14g <ottiagos@gmail.com>
2020-04-06 03:47:56 -0300

    Bckp

---
+++ app/js/script.js
@@ -0,0 +1,24 @@
+console.log("Load script11.js");
+
+// Instantiating the global app object
+var app = {};
+
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+//Grid
